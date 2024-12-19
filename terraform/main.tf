3336bd07749b2bfba840dacc98882e56448defea
SendoDZ <Icham.benabbas@epita.fr>
2024-12-19 06:33:54 +0100

    l

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "servernode" {
-  ami                    = "ami-075449515af5df0d1"
+  ami                    = "ami-089146c5626baa6bf"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
